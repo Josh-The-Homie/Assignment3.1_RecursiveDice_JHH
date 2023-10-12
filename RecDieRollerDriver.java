f4e743c8f991fdaf0332b2541532fb47cf58bb7c
Josh-Henderson <122053780+Josh-The-Homie@users.noreply.github.com>
2023-10-11 21:12:25 -0400

    RecDieRollerDriver.java

--- RecDieRollerDriver.java
+++ RecDieRollerDriver.java
@@ -1,3 +1,7 @@
+/**
+@author Joshua Henderson
+This program instantiates the RecDieRoller class and rolls it 1000 times counting the amount of twos.
+*/
 public class RecDieRollerDriver {
     public static void main(String[] args) {
         int totalTwos = 0;
@@ -7,5 +11,5 @@ public class RecDieRollerDriver {
         totalTwos = roller.countTwos(numRolls); // Count how many times 2 is rolled in 1,000 rolls
 
         System.out.println("Number of 2s rolled after " + numRolls + " rolls: " + totalTwos);
-    }
-}
+    }//end main
+}//end RecDieRollerDriver
