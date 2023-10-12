9a76a8f44c7df33127087d4da86d60acf7114487
Josh-Henderson <122053780+Josh-The-Homie@users.noreply.github.com>
2023-10-11 21:10:12 -0400

    Update RecDieRoller.java

--- RecDieRoller.java
+++ RecDieRoller.java
@@ -1,3 +1,7 @@
+/**
+@author Joshua Henderson 
+This program rolls a dice until it returns a two.
+*/
 public class RecDieRoller {
     public int countTwos(int numRolls) {
         int twos = 0;
@@ -5,8 +9,8 @@ public class RecDieRoller {
             int die = (int)(Math.random() * 6) + 1;
             if (die == 2) {
                 twos++;
-            }
-        }
+            }//end if
+        }//end for
         return twos;
-    }
-}
+    }//end countTwos
+}//end RecDieRoller
